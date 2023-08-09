4377a1076cbb468f7056be3035f00a54146d7a55
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-09 19:43:32 +0200

    Renamed

---
+++ media-server/src/main/java/com/petarj123/mediaserver/Application.java
@@ -0,0 +1,13 @@
+package com.petarj123.mediaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
