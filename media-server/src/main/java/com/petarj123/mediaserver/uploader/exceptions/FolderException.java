a88bde0938695ea1b73059f282f06ca9950745a0
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-10 11:50:02 +0200

    Added custom exceptions and exception handler

---
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/exceptions/FolderException.java
@@ -0,0 +1,7 @@
+package com.petarj123.mediaserver.uploader.exceptions;
+
+public class FolderException extends Exception {
+    public FolderException(String message) {
+        super(message);
+    }
+}
---
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/exceptions/UploaderExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.petarj123.mediaserver.uploader.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class UploaderExceptionHandler {
+    // TODO Make DTOs for exceptions
+    @ExceptionHandler(FileException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleFileException(FileException ex) {
+        return ex.getMessage();
+    }
+    @ExceptionHandler(FolderException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleFolderException(FolderException ex) {
+        return ex.getMessage();
+    }
+}
