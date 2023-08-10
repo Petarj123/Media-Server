7bd732e4dc6575c7a0c3bb645bbc7ec6b9e74ba5
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-10 12:13:10 +0200

    Update UploaderExceptionHandler.java

--- media-server/src/main/java/com/petarj123/mediaserver/uploader/exceptions/UploaderExceptionHandler.java
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/exceptions/UploaderExceptionHandler.java
@@ -3,6 +3,7 @@ package com.petarj123.mediaserver.uploader.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
@@ -10,11 +11,13 @@ public class UploaderExceptionHandler {
     // TODO Make DTOs for exceptions
     @ExceptionHandler(FileException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
     public String handleFileException(FileException ex) {
         return ex.getMessage();
     }
     @ExceptionHandler(FolderException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
     public String handleFolderException(FolderException ex) {
         return ex.getMessage();
     }
