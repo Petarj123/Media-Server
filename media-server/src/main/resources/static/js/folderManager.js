20977e276d0ac2ef97783e1aa4ecb37a5bd87638
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-10 11:50:26 +0200

    Frontend update

--- media-server/src/main/resources/static/js/folderManager.js
+++ media-server/src/main/resources/static/js/folderManager.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         fetch('/folder/create', {
             method: 'POST',
-            body: new URLSearchParams({ 'name': folderName }),
+            body: JSON.stringify({ name: folderName }),
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         fetch('/folder/delete', {
             method: 'DELETE',
-            body: new URLSearchParams({ 'folderName': folderName }),
+            body: JSON.stringify({ name: folderName }),
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -56,12 +56,19 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    // Upload image (add this if you want a separate function for it)
+    // Upload files
     document.getElementById("uploadImageButton").addEventListener('click', function() {
         const form = document.getElementById("uploadImageForm");
-        const formData = new FormData(form);
+        const formData = new FormData();
 
-        fetch('/upload', {
+        // Add each file to the form data
+        const files = form.querySelector('input[type="file"]').files;
+        for (let i = 0; i < files.length; i++) {
+            formData.append('files', files[i]);
+        }
+        formData.append('folderName', form.querySelector('input[name="folderName"]').value);
+
+        fetch('/file/upload', {
             method: 'POST',
             body: formData,
         })
@@ -69,17 +76,57 @@ document.addEventListener("DOMContentLoaded", function() {
             if (response.ok) {
                 return response.json();
             }
-            throw new Error('Failed to upload image');
+            throw new Error('Failed to upload image(s)');
         })
         .then(data => {
             console.log(data);
-            // Handle the successful upload of an image (e.g., update the UI)
+            // Handle the successful upload of images (e.g., update the UI)
         })
         .catch(error => {
             console.error('Error:', error);
         });
     });
 
+    // Fetch all folders and populate the folderList div
+        fetch('/folder/all')
+        .then(response => response.json())
+        .then(data => {
+            const folderList = document.getElementById('folderList');
+            data.forEach(folder => {
+                const folderElement = document.createElement('div');
+                folderElement.className = 'folder-item';
+                folderElement.innerText = `${folder.name} (Last Modified: ${new Date(folder.lastModified).toLocaleDateString()})`;
+
+                const filesList = document.createElement('ul');
+                filesList.style.display = 'none'; // Initially hidden
+
+                folderElement.addEventListener('click', function() {
+                    // Check if filesList is already populated
+                    if (filesList.childNodes.length) {
+                        // If it has children (i.e., it's populated), toggle the display
+                        if(filesList.style.display === 'none') {
+                            filesList.style.display = 'block';
+                        } else {
+                            filesList.style.display = 'none';
+                        }
+                    } else {
+                        // If not populated, fetch files and display
+                        fetch(`/folder/${folder.name}/files`)
+                        .then(response => response.json())
+                        .then(files => {
+                            files.forEach(fileName => {
+                                const listItem = document.createElement('li');
+                                listItem.innerText = fileName;
+                                filesList.appendChild(listItem);
+                            });
+                            folderElement.appendChild(filesList);
+                            filesList.style.display = 'block'; // Show the files list
+                        });
+                    }
+                });
+                folderList.appendChild(folderElement);
+            });
+        });
 });
 
 
