111d47702a853ceb6655264951e44fdec89e2be8
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-11 19:30:40 +0200

    Updated SecurityConfig and created SecureUser class

--- media-server/src/main/java/com/petarj123/mediaserver/auth/config/SecurityConfig.java
+++ media-server/src/main/java/com/petarj123/mediaserver/auth/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.petarj123.mediaserver.auth.config;
 
+import com.petarj123.mediaserver.auth.jwt.config.AuthenticationFilter;
+import com.petarj123.mediaserver.auth.jwt.config.JwtAuthenticationEntryPoint;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -7,33 +9,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @AllArgsConstructor
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final AuthenticationFilter authenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final AuthenticationManager authenticationManager;
+
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
                 .authorizeHttpRequests((authorize) -> {
                     authorize.requestMatchers("/api/auth/**").permitAll();
                     authorize.anyRequest().authenticated();
-                });
+                })
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
 }
--- media-server/src/main/java/com/petarj123/mediaserver/auth/jwt/service/CustomUserDetails.java
+++ media-server/src/main/java/com/petarj123/mediaserver/auth/jwt/service/CustomUserDetails.java
@@ -1,17 +1,13 @@
 package com.petarj123.mediaserver.auth.jwt.service;
 
+import com.petarj123.mediaserver.auth.user.model.SecureUser;
 import com.petarj123.mediaserver.auth.user.model.User;
 import com.petarj123.mediaserver.auth.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -21,12 +17,9 @@ public class CustomUserDetails implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException("No user found with username or email: " + usernameOrEmail));
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username or email: " + usernameOrEmail));
 
-        Set<GrantedAuthority> authorities = user.getRoles().stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
-                .collect(Collectors.toSet());
-
-        return new org.springframework.security.core.userdetails.User(usernameOrEmail, user.getPassword(), authorities);
+        return new SecureUser(user);
     }
 }
