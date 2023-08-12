fbc4d831cd7be09d40108860d225ce3f41264192
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-12 16:00:06 +0200

    Create InfectedFileException.java

---
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/exceptions/InfectedFileException.java
@@ -0,0 +1,7 @@
+package com.petarj123.mediaserver.uploader.exceptions;
+
+public class InfectedFileException extends RuntimeException {
+    public InfectedFileException(String message) {
+        super(message);
+    }
+}
