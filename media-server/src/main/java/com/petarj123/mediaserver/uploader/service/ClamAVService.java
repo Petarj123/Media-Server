a393e2bb8c90aba6dd95c0e50d37f7804f047365
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-12 16:42:22 +0200

    Added scheduled scans

--- media-server/src/main/java/com/petarj123/mediaserver/uploader/service/ClamAVService.java
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/service/ClamAVService.java
@@ -1,14 +1,22 @@
 package com.petarj123.mediaserver.uploader.service;
 
 import com.petarj123.mediaserver.uploader.DTO.ScanResult;
+import com.petarj123.mediaserver.uploader.exceptions.InfectedFileException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 @Service
 public class ClamAVService {
@@ -19,6 +27,8 @@ public class ClamAVService {
     @Value("${clamav.port}")
     private int clamAVPort;
 
+    private static final String serverFolderPath = "/home/petarjankovic/Documents/Server/";
+    private static final Logger logger = LoggerFactory.getLogger(ClamAVService.class);
 
     public ScanResult scanFile(Path file) throws IOException {
         String responseString;
@@ -42,6 +52,26 @@ public class ClamAVService {
         return new ScanResult(file.toString(), isClean);
     }
 
-    // TODO Sanitize File Names, File Size Limit, Scheduled Scans, Retry Mechanism, Asynchronous Scanning, Batch Scanning
+    // TODO Sanitize File Names, File Size Limit, Retry Mechanism, Asynchronous Scanning, Batch Scanning
+    @Scheduled(fixedRate = 24 * 60 * 60 * 1000)
+    public void scanServer() {
+        try (Stream<Path> paths = Files.walk(Paths.get(serverFolderPath))) {
+            paths
+                    .filter(Files::isRegularFile) // Only scan files, not directories
+                    .forEach(file -> {
+                        try {
+                            ScanResult scanResult = scanFile(file);
+                            if (!scanResult.isClean()) {
+                                Files.delete(file);
+                                throw new InfectedFileException("File " + file.getFileName() + " is infected and has been deleted.");
+                            }
+                        } catch (IOException e) {
+                            logger.error("Error scanning or deleting file: {}", file.getFileName(), e);
+                        }
+                    });
+        } catch (IOException e) {
+            logger.error("Error walking through server folder path", e);
+        }
+    }
 
 }
