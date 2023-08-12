85f8a4979ae62148a67bfbc07051be06ae8dffa0
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-12 16:41:46 +0200

    Create InvalidFileExtensionException.java

---
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/exceptions/InvalidFileExtensionException.java
@@ -0,0 +1,7 @@
+package com.petarj123.mediaserver.uploader.exceptions;
+
+public class InvalidFileExtensionException extends Throwable {
+    public InvalidFileExtensionException(String msg) {
+        super(msg);
+    }
+}
