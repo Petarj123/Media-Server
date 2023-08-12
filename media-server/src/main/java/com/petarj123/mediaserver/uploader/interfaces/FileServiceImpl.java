080b13615a1faf02072bf8b34e83e1790cd7f9f0
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-12 15:59:54 +0200

    Update FileServiceImpl.java

--- media-server/src/main/java/com/petarj123/mediaserver/uploader/interfaces/FileServiceImpl.java
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/interfaces/FileServiceImpl.java
@@ -1,10 +1,11 @@
 package com.petarj123.mediaserver.uploader.interfaces;
 
+import com.petarj123.mediaserver.uploader.DTO.ScanResult;
 import com.petarj123.mediaserver.uploader.exceptions.FileException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileServiceImpl {
-    String saveFile(MultipartFile file, String folderName) throws FileException;
+    ScanResult saveFile(MultipartFile file, String folderName) throws FileException;
     boolean deleteFile(String filename, String folderName) throws FileException;
 
 }
