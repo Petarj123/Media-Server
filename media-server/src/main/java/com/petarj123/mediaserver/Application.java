74256b42352d23a754fc46709a1d7240a42a9508
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-12 16:41:49 +0200

    Update Application.java

--- media-server/src/main/java/com/petarj123/mediaserver/Application.java
+++ media-server/src/main/java/com/petarj123/mediaserver/Application.java
@@ -2,8 +2,10 @@ package com.petarj123.mediaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
