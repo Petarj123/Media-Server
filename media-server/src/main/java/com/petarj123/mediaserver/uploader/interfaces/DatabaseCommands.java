babfc8a8099f02c73d6945abd7932af883fe42a5
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-14 19:36:15 +0200

    Update DatabaseCommands.java

--- media-server/src/main/java/com/petarj123/mediaserver/uploader/interfaces/DatabaseCommands.java
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/interfaces/DatabaseCommands.java
@@ -1,8 +1,6 @@
 package com.petarj123.mediaserver.uploader.interfaces;
 
-import org.springframework.scheduling.annotation.Scheduled;
-
 public interface DatabaseCommands {
-    void connect(String mongodbUri, String database);
+    void setDBConnection(String mongodbUri, String database);
     void backupDatabase();
 }
