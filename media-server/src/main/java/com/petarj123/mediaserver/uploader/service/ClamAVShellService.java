e9535ea706b4385d0d116088c7f6a8c417458f3a
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-15 17:44:14 +0200

    Implemented ClamAVShellServiceImpl

--- media-server/src/main/java/com/petarj123/mediaserver/uploader/service/ClamAVShellService.java
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/service/ClamAVShellService.java
@@ -1,44 +1,156 @@
 package com.petarj123.mediaserver.uploader.service;
 
 import com.petarj123.mediaserver.uploader.interfaces.ClamAVShellServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.time.LocalDateTime;
+import java.util.Properties;
 
 @Service
+@RequiredArgsConstructor
 public class ClamAVShellService implements ClamAVShellServiceImpl {
+    @Value("${properties.path}")
+    private String PROP_FILE_PATH;
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseShellService.class);
+    private final ClamAVService clamAVService;
+
     @Override
     public void enableClamAV() {
+        try{
+            Properties properties = new Properties();
+            FileInputStream in = new FileInputStream(PROP_FILE_PATH);
+            properties.load(in);
+            in.close();
 
+            properties.setProperty("clamav.enabled", "true");
+
+            FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
+            properties.store(out, null);
+            out.close();
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
     }
 
     @Override
     public void disableClamAV() {
+        try{
+            Properties properties = new Properties();
+            FileInputStream in = new FileInputStream(PROP_FILE_PATH);
+            properties.load(in);
+            in.close();
 
+            properties.setProperty("clamav.enabled", "false");
+
+            FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
+            properties.store(out, null);
+            out.close();
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
     }
 
     @Override
     public void enableScans() {
+        try{
+            Properties properties = new Properties();
+            FileInputStream in = new FileInputStream(PROP_FILE_PATH);
+            properties.load(in);
+            in.close();
 
+            properties.setProperty("clamav.scans.enabled", "true");
+
+            FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
+            properties.store(out, null);
+            out.close();
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
     }
 
     @Override
     public void disableScans() {
+        try{
+            Properties properties = new Properties();
+            FileInputStream in = new FileInputStream(PROP_FILE_PATH);
+            properties.load(in);
+            in.close();
 
+            properties.setProperty("clamav.scans.enabled", "false");
+
+            FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
+            properties.store(out, null);
+            out.close();
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
     }
 
     @Override
-    public void scheduleScans(LocalDateTime time) {
-
+    public void scheduleScans(long ms) {
+        setScanInterval(ms);
     }
 
     @Override
     public void setPort(int port) {
+        try{
+            Properties properties = new Properties();
+            FileInputStream in = new FileInputStream(PROP_FILE_PATH);
+            properties.load(in);
+            in.close();
 
+            properties.setProperty("clamav.port", String.valueOf(port));
+
+            FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
+            properties.store(out, null);
+            out.close();
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
     }
 
     @Override
     public void setHost(String host) {
+        try{
+            Properties properties = new Properties();
+            FileInputStream in = new FileInputStream(PROP_FILE_PATH);
+            properties.load(in);
+            in.close();
 
+            properties.setProperty("clamav.host", host);
+
+            FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
+            properties.store(out, null);
+            out.close();
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+    }
+
+    private void setScanInterval(long interval) {
+        try {
+            Properties properties = new Properties();
+            FileInputStream in = new FileInputStream(PROP_FILE_PATH);
+            properties.load(in);
+            in.close();
+
+            properties.setProperty("scan.interval", String.valueOf(interval));
+
+            FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
+            properties.store(out, null);
+            out.close();
+
+            // Reschedule the scanning task with the new interval
+            clamAVService.startScheduledTask();
+
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
     }
 }
