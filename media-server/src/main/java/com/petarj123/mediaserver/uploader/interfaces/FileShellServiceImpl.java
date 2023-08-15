7e9f3dbc1ee22adf29a5a56f48423e1d1bfc495e
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-15 17:43:28 +0200

    Update FileShellServiceImpl.java

--- media-server/src/main/java/com/petarj123/mediaserver/uploader/interfaces/FileShellServiceImpl.java
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/interfaces/FileShellServiceImpl.java
@@ -1,8 +1,14 @@
 package com.petarj123.mediaserver.uploader.interfaces;
 
+import com.petarj123.mediaserver.uploader.exceptions.InvalidFileExtensionException;
+import com.petarj123.mediaserver.uploader.file.model.FileType;
+
+import java.util.List;
+
 public interface FileShellServiceImpl {
     void setMaxFileSize(long size);
     void setMaxRequestSize(long size);
 
-    void setAllowedExtensions(String... extensions);
+    void setAllowedExtensions(FileType type, List<String> extensions) throws InvalidFileExtensionException;
+    void removeExtension(FileType type, String extension) throws InvalidFileExtensionException;
 }
