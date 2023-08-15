997122de32dc63d6eb65c7361890a807c86c9df1
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-15 17:43:13 +0200

    Update FileShellService.java

--- media-server/src/main/java/com/petarj123/mediaserver/uploader/file/service/FileShellService.java
+++ media-server/src/main/java/com/petarj123/mediaserver/uploader/file/service/FileShellService.java
@@ -1,7 +1,12 @@
 package com.petarj123.mediaserver.uploader.file.service;
 
+import com.petarj123.mediaserver.uploader.exceptions.InvalidFileExtensionException;
+import com.petarj123.mediaserver.uploader.file.model.Extension;
+import com.petarj123.mediaserver.uploader.file.model.FileType;
+import com.petarj123.mediaserver.uploader.file.repository.ExtensionRepository;
 import com.petarj123.mediaserver.uploader.interfaces.FileShellServiceImpl;
 import com.petarj123.mediaserver.uploader.service.DatabaseShellService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,23 +14,27 @@ import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.List;
 import java.util.Properties;
 
 @Service
+@RequiredArgsConstructor
 public class FileShellService implements FileShellServiceImpl {
     @Value("${properties.path}")
     private String PROP_FILE_PATH;
     private static final Logger logger = LoggerFactory.getLogger(DatabaseShellService.class);
-
+    private final FileSanitizer fileSanitizer;
+    private final ExtensionRepository extensionRepository;
     @Override
-    public void setMaxFileSize(long size) {
+    public void setMaxFileSize(long mb) {
         try {
             Properties properties = new Properties();
             FileInputStream in = new FileInputStream(PROP_FILE_PATH);
             properties.load(in);
             in.close();
 
-            properties.setProperty("spring.servlet.multipart.max-file-size", Long.toString(size));
+            String size = mb + "MB";
+            properties.setProperty("spring.servlet.multipart.max-file-size", size);
 
             FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
             properties.store(out, null);
@@ -36,14 +45,15 @@ public class FileShellService implements FileShellServiceImpl {
     }
 
     @Override
-    public void setMaxRequestSize(long size) {
+    public void setMaxRequestSize(long mb) {
         try {
             Properties properties = new Properties();
             FileInputStream in = new FileInputStream(PROP_FILE_PATH);
             properties.load(in);
             in.close();
 
-            properties.setProperty("spring.servlet.multipart.max-request-size", Long.toString(size));
+            String size = mb + "MB";
+            properties.setProperty("spring.servlet.multipart.max-request-size", size);
 
             FileOutputStream out = new FileOutputStream(PROP_FILE_PATH);
             properties.store(out, null);
@@ -54,7 +64,20 @@ public class FileShellService implements FileShellServiceImpl {
     }
 
     @Override
-    public void setAllowedExtensions(String... extensions) {
-        // TODO
+    public void setAllowedExtensions(FileType fileType, List<String> extensions) throws InvalidFileExtensionException {
+        Extension ext = extensionRepository.findByFileType(fileType).orElseThrow(() -> new InvalidFileExtensionException("Unsupported file type"));
+        List<String> currentExtensions = ext.getExtension();
+        currentExtensions.addAll(extensions);
+        ext.setExtension(currentExtensions);
+        extensionRepository.save(ext);
+    }
+
+    @Override
+    public void removeExtension(FileType fileType, String extension) throws InvalidFileExtensionException {
+        Extension ext = extensionRepository.findByFileType(fileType).orElseThrow(() -> new InvalidFileExtensionException("Unsupported file type"));
+        List<String> currentExtensions = ext.getExtension();
+        currentExtensions.removeIf(ex -> ex.equals(extension));
+        ext.setExtension(currentExtensions);
+        extensionRepository.save(ext);
     }
 }
