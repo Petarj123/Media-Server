ba1f11fcc90b7dad3db8735b044d458e7013ca16
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-21 16:53:16 +0200

    Update JwtImpl.java

--- media-server/src/main/java/com/petarj123/mediaserver/auth/interfaces/JwtImpl.java
+++ media-server/src/main/java/com/petarj123/mediaserver/auth/interfaces/JwtImpl.java
@@ -7,6 +7,7 @@ import java.security.Key;
 public interface JwtImpl {
     String generateToken(Authentication authentication);
     String getUsername(String token);
+    String getEmail(String token);
     boolean validateToken(String token);
 
 }
