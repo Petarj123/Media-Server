486635fa592856f3e163a6eda12627472c174f9f
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-08-25 12:04:18 +0200

    Update Application.java

--- media-server/src/main/java/com/petarj123/mediaserver/Application.java
+++ media-server/src/main/java/com/petarj123/mediaserver/Application.java
@@ -2,6 +2,7 @@ package com.petarj123.mediaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
